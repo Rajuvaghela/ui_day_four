650a85b1fa815a608a16868ef2b799ba4f39904f
Raju <rajuvaghela1485@gmail.com>
2025-04-11 21:54:36 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ui_day_four/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ui_day_four
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ui_day_four/root_app.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Profile Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: const RootApp(),
+    );
+  }
+}
